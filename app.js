073f6031602283ad9bdf8ced2edd30d46dc53675
Yodit Teamir <79291410+Yoditatr@users.noreply.github.com>
2021-07-18 13:58:22 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+// console.log("Hello from the COVID playbook!")
+let viz; 
+
+const url = 
+
+"https://public.tableau.com/views/MarylandCoviddemo/DEMO-MarylandCovidcasesdashboard?"
+
+const vizContainer = document.getElementById("vizContainer");
+
+const exportPDFButton = document.getElementById("exportPDF");
+
+function initViz() {
+
+    viz = new tableau.Viz(vizContainer, url);
+
+}
+
+function exportToPDF() {
+
+    viz.showExportPDFDialog();
+}
+
+exportPDFButton.addEventListener("click", exportToPDF);
+
+document.addEventListener("DOMContentLoaded", initViz);
+
